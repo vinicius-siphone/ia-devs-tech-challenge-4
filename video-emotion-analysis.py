1ac70ff42ee5e08c05adbf6254b269ee33e3c25a
vinicius-siphone <vinicius.siphone@icloud.com>
2026-03-03 21:33:05 -0300

    refactor: remove unnecessary code and rename audio file

--- video-emotion-analysis.py
+++ video-emotion-analysis.py
@@ -54,69 +54,7 @@ def load_images_from_folder(folder):
 
     return known_face_encodings, known_face_names
 
-# 2. Cálculo do score de risco psicológico com base no histórico de emoções
-def calculate_risk_score(emotions_history):
-    score = 0
-
-    for emo in emotions_history:
-        score += EMOTION_WEIGHTS.get(emo, 0)
-
-    avg_score = score / len(emotions_history)
-
-    return round(avg_score, 2)
-    # Capturar vídeo do arquivo especificado
-    cap = cv2.VideoCapture(video_path)
-
-    # Verificar se o vídeo foi aberto corretamente
-    if not cap.isOpened():
-        print("Erro ao abrir o vídeo.")
-        return
-
-    # Obter propriedades do vídeo
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    # Definir o codec e criar o objeto VideoWriter
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec para MP4
-    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-    # Loop para processar cada frame do vídeo
-    for _ in tqdm(range(total_frames), desc="Processando vídeo"):
-        # Ler um frame do vídeo
-        ret, frame = cap.read()
-
-        # Se não conseguiu ler o frame (final do vídeo), sair do loop
-        if not ret:
-            break
-
-        # Analisar o frame para detectar faces e expressões
-        result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
-
-        # Iterar sobre cada face detectada
-        for face in result:
-            # Obter a caixa delimitadora da face
-            x, y, w, h = face['region']['x'], face['region']['y'], face['region']['w'], face['region']['h']
-            
-            # Obter a emoção dominante
-            dominant_emotion = face['dominant_emotion']
-            
-            # Desenhar um retângulo ao redor da face
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            
-            # Escrever a emoção dominante acima da face
-            cv2.putText(frame, dominant_emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
-        
-        # Escrever o frame processado no vídeo de saída
-        out.write(frame)
-
-    # Liberar a captura de vídeo e fechar todas as janelas
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-# 3. Função que detecta cada face e de cada face detecta as emoções
+# 2. Função que detecta cada face e de cada face detecta as emoções
 def detect_faces_and_emotions(video_path, output_path, known_face_encodings, known_face_names):
     # Capturar vídeo do arquivo especificado
     cap = cv2.VideoCapture(video_path)
@@ -194,8 +132,8 @@ def detect_faces_and_emotions(video_path, output_path, known_face_encodings, kno
     out.release()
     cv2.destroyAllWindows()
 
-# 4. Carregar imagens e codificações
+# 3. Carregar imagens e codificações
 known_face_encodings, known_face_names = load_images_from_folder(image_folder)
 
-# 5. Chamar a função para detectar emoções e reconhecer faces no vídeo, salvando o vídeo processado
+# 4. Chamar a função para detectar emoções e reconhecer faces no vídeo, salvando o vídeo processado
 detect_faces_and_emotions(input_video, output_video, known_face_encodings, known_face_names)
